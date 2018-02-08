af9ceccf25f0247326252654cc84c7d51a74d04b
steventeddy <stlan@ucdavis.edu>
2018-02-08 11:37:14 -0800

    part i: finished- add unit circle plot

--- part_i.m
+++ part_i.m
@@ -1,4 +1,4 @@
-function [M] = part_i()
+function [M1, M2] = part_i()
 %part_i Contains code for part (i) of the project
 %   A copy of the code on p.100 of Greenbaum and Chartier
     
@@ -7,9 +7,7 @@ phi = inline('z^2 - 1.25');          % Define the function whose fixed points we
 fixpt1 = (1 + sqrt(6))/2;     % These are the fixed points.
 fixpt2 = (1 - sqrt(6))/2;
 
-colormap([1 0 0; 1 1 1]);     % Points numbered 1 (inside) will be colored red;
-                              %   those numbered 2 (outside) will be colored white.
-M = 2*ones(141,361);          % Initialize array of point colors to 2 (white).
+M1 = 2*ones(141,361);          % Initialize array of point colors to 2 (white).
 
 for j=1:141                   % Try initial values with imaginary parts between
   y = -.7 + (j-1)*.01;        %   -0.7 and 0.7
@@ -38,7 +36,41 @@ for j=1:141                   % Try initial values with imaginary parts between
       end
     end
     if iflag1 >= 5 | iflag2 >= 5 | kount >= 100   % If orbit is bounded, set this
-      M(j,i) = 1;                                  %   point color to 1 (red).
+      M1(j,i) = 1;                                  %   point color to 1 (red).
+    end
+  end
+end
+
+% code for the unit circle a la c = 0.
+
+phi = inline('z^2');          % Define the function whose fixed points we seek.
+
+fixpt1 = 0;     % This is the fixed point.
+
+M2 = 2*ones(200,200);          % Initialize array of point colors to 2 (white).
+
+for j=1:200                  % Try initial values with imaginary parts between
+  y = -1 + (j-1)*.01;        %   -0.7 and 0.7
+  for i=1:200                 % and with real parts between
+    x = -1+ (i-1)*.01;     %   -1.8 and 1.8.
+    z = x + 1i*y;             % 1i is the MATLAB symbol for sqrt(-1).
+    zk = z;
+    iflag1 = 0;               % iflag1 count the number of iterations
+                              % when a root is within 1.e-6 of a fixed point;
+    kount = 0;                % kount is the total number of iterations.
+
+    while kount < 100 & abs(zk) < 2 & iflag1 < 5 
+      kount = kount+1;
+      zk = phi(zk);           % This is the fixed point iteration.
+      err1 = abs(zk-fixpt1);  % Test for convergence to fixpt1.
+      if err1 < 1.e-6 
+         iflag1 = iflag1 + 1;
+      else
+         iflag1 = 0;
+      end
+    end
+    if iflag1 >= 5 | kount >= 100   % If orbit is bounded, set this
+      M2(j,i) = 1;                  %   point color to 1 (red).
     end
   end
 end
--- proj_main.m
+++ proj_main.m
@@ -6,8 +6,17 @@ clear;
 %% Part i
 
 % the code for plotting has been moved outside of the function 
-M = part_i();
+[M1, M2] = part_i();
 figure(1);
-image([-1.8 1.8],[-.7 .7],M),  % This plots the results.
+colormap([1 0 0; 1 1 1]);      % Set the color map, 1 is red, 2 is white.
+image([-1.8 1.8],[-.7 .7],M1), % This plots the results.
 axis xy                        % If you don't do this, vertical axis is inverted.
 
+
+figure(2);
+colormap([1 0 0; 1 1 1]); % Set the color map, 1 is red, 2 is white
+image([-1 1],[-1 1],M2),  % This plots the results.
+axis equal;               % makes the image square.
+axis xy                   % If you don't do this, vertical axis is inverted.
+
+%% Part ii
