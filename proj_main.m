a4bb8c60b530f2ac2821e3502a1f27fde2960af1
steventeddy <stlan@ucdavis.edu>
2018-02-08 12:08:24 -0800

    part ii: initial commit with EX of c=0.36+0.1i

--- proj_main.m
+++ proj_main.m
@@ -14,9 +14,15 @@ axis xy                        % If you don't do this, vertical axis is inverted
 
 
 figure(2);
-colormap([1 0 0; 1 1 1]); % Set the color map, 1 is red, 2 is white
-image([-1 1],[-1 1],M2),  % This plots the results.
+colormap([1 0 0; 1 1 1]); 
+image([-1 1],[-1 1],M2),  
 axis equal;               % makes the image square.
-axis xy                   % If you don't do this, vertical axis is inverted.
+axis xy                   
 
 %% Part ii
+% parfor is used in the interest of runtime.
+M3 = part_ii();
+figure(2);
+colormap([1 0 0; 1 1 1]);      
+image([-1.8 1.8],[-.7 .7],M3), 
+axis xy                        
